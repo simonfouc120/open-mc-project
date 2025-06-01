8efc0640e38a2013ac0ec00663f261d0b068185d
simonfouc120 <simon.fcbrt@gmail.com>
2025-06-01 18:43:08 +0200

    feat:add_photons_cartography

--- src/studies/lattice_fuel.py/core.py
+++ src/studies/lattice_fuel.py/core.py
@@ -15,11 +15,12 @@ CWD = Path(__file__).parent.resolve()
 project_root = Path(__file__).resolve().parents[3]  
 sys.path.append(str(project_root))
 from parameters.parameters_paths import PATH_TO_CROSS_SECTIONS
-from parameters.parameters_materials import FUEL_MATERIAL, CDTE_MATERIAL, AIR_MATERIAL, CONCRETE_MATERIAL, GRAPHITE_MATERIAL, URANIUM_MATERIAL
+from parameters.parameters_materials import FUEL_MATERIAL, HELIUM_MATERIAL, AIR_MATERIAL, CONCRETE_MATERIAL, GRAPHITE_MATERIAL, STEEL_MATERIAL
 from src.utils.pre_processing.pre_processing import remove_previous_results, parallelepiped, plot_geometry, mesh_tally
+from src.utils.post_preocessing.post_processing import load_mesh_tally
 os.environ["OPENMC_CROSS_SECTIONS"] = PATH_TO_CROSS_SECTIONS
 
-materials = openmc.Materials([FUEL_MATERIAL, CDTE_MATERIAL, AIR_MATERIAL, CONCRETE_MATERIAL, GRAPHITE_MATERIAL])
+materials = openmc.Materials([FUEL_MATERIAL, HELIUM_MATERIAL, AIR_MATERIAL, CONCRETE_MATERIAL, GRAPHITE_MATERIAL, STEEL_MATERIAL])
 
 material = openmc.Materials((FUEL_MATERIAL, GRAPHITE_MATERIAL, AIR_MATERIAL))
 
@@ -42,13 +43,13 @@ lat.pitch = (1.25,)   # pitch en cm
 lat.outer = outer_universe
 
 lat.universes = [
-    [pin_universe] * 18,    # 1st ring
+    [big_pin_universe] * 18,    # 1st ring
     [pin_universe] * 12,    # 2nd ring
     [big_pin_universe] * 6, # 3rd ring
     [big_pin_universe]      # 4th ring
 ]
 
-outer_surface = openmc.ZCylinder(r=4.5)
+outer_surface = openmc.ZCylinder(r=5.5)
 height_top = openmc.ZPlane(z0=30.0)
 height_bottom = openmc.ZPlane(z0=-30.0)
 main_cell = openmc.Cell(
@@ -72,56 +73,59 @@ air_cell_side = openmc.Cell(fill=AIR_MATERIAL, region=air_region_side)
 
 geometry = openmc.Geometry([main_cell, air_cell_above, air_cell_below, air_cell_side])
 
-plot_geometry(materials = materials, plane="xy")
+plot_geometry(materials = materials, plane="xy", width=12, height=12)
 
-plot_geometry(materials = materials, plane="xz")
+plot_geometry(materials = materials, plane="xz", width=12, height=12)
 
 plot_geometry(materials = materials, plane="yz", width=70, height=70)
 
- 
 # Calcul de criticité simple 
 settings = openmc.Settings()
-batches_number= 50
+batches_number= 100
 settings.batches = batches_number
 settings.inactive = 20
 settings.particles = 10000
 settings.source = openmc.Source()
 settings.source.space = openmc.stats.Point((0, 0, 0))
 settings.source.particle = 'neutron'
+settings.photon_transport = True
 settings.source.angle = openmc.stats.Isotropic()
-
 
 # tally pour le flux dans le détecteur
 tally = openmc.Tally(name="flux_tally")
 tally.scores = ['flux']
 tally.filters = [openmc.CellFilter(main_cell)]
 tallies = openmc.Tallies([tally])
-# tallies.append(tally)
 
-tallies.append(mesh_tally(particule_type='neutron', bin_number=400, lower_left=(-10.0, -10.0), upper_right=(10.0, 10.0)))
+# Mesh tally for neutron flux in a specific region
+mesh_tally_neutron = mesh_tally(name_mesh_tally = "flux_mesh_neutrons", particule_type='neutron', bin_number=400, lower_left=(-10.0, -10.0), upper_right=(10.0, 10.0))
+tallies.append(mesh_tally_neutron)
+
+mesh_tally_photon = mesh_tally(name_mesh_tally = "flux_mesh_photons", particule_type='photon', bin_number=400, lower_left=(-10.0, -10.0), upper_right=(10.0, 10.0))
+tallies.append(mesh_tally_photon)
+
 
 geometry.export_to_xml()
 material.export_to_xml()
 settings.export_to_xml()
 tallies.export_to_xml()
 
-
 # Run the simulation
-
 remove_previous_results(batches_number=batches_number)
 openmc.run()
 
-
 # Load the statepoint file
 statepoint_file = openmc.StatePoint(f'statepoint.{batches_number}.h5')
-# Get the tally results
 tally = statepoint_file.get_tally(name="flux_tally")
-# Get the mean flux value
 mean_flux = tally.mean.flatten()
-# Get the standard deviation of the flux
 std_flux = tally.std_dev.flatten()
-# Print the results
 print(f"Mean flux: {mean_flux}")
 print(f"Standard deviation of flux: {std_flux}")
 
 
+### mesh tallty ####
+# Récupérer le tally du maillage
+
+load_mesh_tally(cwd = CWD, statepoint_file = statepoint_file, name_mesh_tally="flux_mesh_neutrons")
+
+load_mesh_tally(cwd = CWD, statepoint_file = statepoint_file, name_mesh_tally="flux_mesh_photons")
--- src/utils/post_preocessing/post_processing.py
+++ src/utils/post_preocessing/post_processing.py
@@ -1,5 +1,7 @@
-
-
+import openmc
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+import numpy as np
 
 def print_calculation_finished():
     """
@@ -8,3 +10,44 @@ def print_calculation_finished():
     print("######################################################")
     print("Calculation finished successfully.")  # TODO: Implement a more sophisticated logging mechanism if needed.
     print("######################################################")
+
+
+def load_mesh_tally(cwd, statepoint_file: object, name_mesh_tally:str = "flux_mesh_tally", 
+                    bin_number:int=400, lower_left:tuple=(-10.0, -10.0), 
+                    upper_right:tuple=(10.0, 10.0), zoom_x:tuple=(-10, 10), 
+                    zoom_y:tuple=(-10.0, 10.0)):
+    """
+    Load and plot the mesh tally from the statepoint file.
+
+    Parameters:
+    - cwd: Path or directory where the mesh tally image will be saved.
+    - statepoint_file: The OpenMC statepoint file object containing the tally data.
+    - name_mesh_tally_saving: Name of the tally (default is "mesh_tally.png").
+    - bin_number: Number of bins for the mesh tally in each dimension (default is 400).
+    - lower_left: Tuple specifying the lower left corner of the mesh (default is (-10.0, -10.0)).
+    - upper_right: Tuple specifying the upper right corner of the mesh (default is (10.0, 10.0)).
+    - zoom_x: Tuple specifying the x-axis limits for zooming (default is (-10, 10)).
+    - zoom_y: Tuple specifying the y-axis limits for zooming (default is (-10.0, 10.0)).
+
+    This function extracts the mesh tally from the statepoint file, reshapes the data,
+    and plots it using matplotlib with a logarithmic color scale. The resulting plot
+    is saved as a PNG file in the specified directory and displayed.
+    """
+    mesh_tally = statepoint_file.get_tally(name=name_mesh_tally)
+    flux_data = mesh_tally.mean.reshape((bin_number, bin_number))
+    plt.imshow(flux_data, 
+            origin='lower', 
+            extent=[lower_left[0], upper_right[1], lower_left[1], upper_right[1]], 
+            cmap='plasma',
+            norm=LogNorm(vmin=np.min(flux_data[flux_data!=0]), vmax=flux_data.max()))  
+    plt.colorbar(label='Flux [a.u.] (log scale)')
+    plt.title('Carte de flux XY (échelle log)')
+    plt.xlabel('X [cm]')
+    plt.ylabel('Y [cm]')
+    plt.tight_layout()
+    plt.xlim(zoom_x[0], zoom_x[1])
+    plt.ylim(zoom_y[0], zoom_y[1])
+    name_mesh_tally_saving = name_mesh_tally + ".png"
+    plt.savefig(cwd / name_mesh_tally_saving)
+    plt.show()
+
