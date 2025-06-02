6b6605e8bdf6f7016a3762a900817efb0d513ecf
simonfouc120 <simon.fcbrt@gmail.com>
2025-06-02 17:12:03 +0200

    feat:add_plane_mesh_tally

--- src/utils/pre_processing/pre_processing.py
+++ src/utils/pre_processing/pre_processing.py
@@ -43,7 +43,8 @@ def parallelepiped(xmin, xmax, ymin, ymax, zmin, zmax, surface_id_start=10):
 def plot_geometry(materials:list, plane:str="xy", 
                   width: float = 10.0, height: float = 10.0, 
                   origin: tuple = (0, 0, 0), 
-                  pixels: tuple = (600, 600)):
+                  pixels: tuple = (600, 600), 
+                  dpi: int = 300):
     """
     Plots the OpenMC geometry in a plane.
     """
@@ -82,22 +83,127 @@ def plot_geometry(materials:list, plane:str="xy",
         np.linspace(0, img.size[1], num=7),
         [f"{y:.1f}" for y in np.linspace(np.min(Y_VALUES), np.max(Y_VALUES), num=7)]
     )
-    plt.xlabel("X (cm)")
-    plt.ylabel("Y (cm)")
+    if plane == "xy":
+        plt.xlabel("X (cm)")
+        plt.ylabel("Y (cm)")
+    elif plane == "xz":
+        plt.xlabel("X (cm)")
+        plt.ylabel("Z (cm)")
+    elif plane == "yz":
+        plt.xlabel("Y (cm)")
+        plt.ylabel("Z (cm)")
+    else:
+        raise ValueError("plane must be 'xy', 'xz', or 'yz'")
+    
     plt.gca().invert_yaxis()
     plt.legend()
-    plt.savefig("plot_" + plane + ".png", dpi=300, bbox_inches='tight')
+    plt.savefig("plot_" + plane + ".png", dpi=dpi, bbox_inches='tight')
+    # remove the openmc plot file
+    os.remove(plot.filename)
     plt.tight_layout()
     plt.show()
 
+def mesh_tally_xy(
+    name_mesh_tally="flux_mesh",
+    particule_type='neutrons',
+    bin_number=400,
+    lower_left=(-50.0, -50.0),
+    upper_right=(50.0, 50.0),
+    z_value:float = 0.0,
+    z_thickness:float = 1.0
+    )-> object:
+    """
+    Create a mesh tally in the XY plane at a specified Z value.
 
-def mesh_tally(name_mesh_tally = "flux_mesh", particule_type='neutrons', 
-               bin_number=400, lower_left=(-50.0, -50.0), upper_right=(50.0, 50.0)):
-    # Mesh tally de dose 
+    Parameters:
+        name_mesh_tally (str): Name of the tally.
+        particule_type (str): Particle type.
+        bin_number (int): Number of bins in each direction.
+        lower_left (tuple): Lower left corner of the mesh (x, y).
+        upper_right (tuple): Upper right corner of the mesh (x, y).
+        z_value (float): Z coordinate of the mesh center.
+        z_thickness (float): Thickness of the mesh in the Z direction.
+
+    Returns:
+        openmc.Tally: The mesh tally object.
+    """
     mesh = openmc.RegularMesh()
-    mesh.dimension = [bin_number, bin_number]  # XY
-    mesh.lower_left = lower_left
-    mesh.upper_right = upper_right
+    mesh.dimension = [bin_number, bin_number, 1]
+    mesh.lower_left = (lower_left[0], lower_left[1], z_value - z_thickness / 2)
+    mesh.upper_right = (upper_right[0], upper_right[1], z_value + z_thickness / 2)
+
+    mesh_filter = openmc.MeshFilter(mesh)
+    mesh_tally = openmc.Tally(name=name_mesh_tally)
+    particle_filter = openmc.ParticleFilter([particule_type])
+    mesh_tally.filters = [mesh_filter, particle_filter]
+    mesh_tally.scores = ['flux']
+    return mesh_tally
+
+def mesh_tally_xz(
+    name_mesh_tally="flux_mesh",
+    particule_type='neutrons',
+    bin_number=400,
+    lower_left=(-50.0, -50.0),
+    upper_right=(50.0, 50.0),
+    y_value:float = 0.0,
+    y_thickness:float = 1.0
+    )-> object:
+    """
+    Create a mesh tally in the XZ plane at a specified Y value.
+
+    Parameters:
+        name_mesh_tally (str): Name of the tally.
+        particule_type (str): Particle type.
+        bin_number (int): Number of bins in each direction.
+        lower_left (tuple): Lower left corner of the mesh (x, z).
+        upper_right (tuple): Upper right corner of the mesh (x, z).
+        y_value (float): Y coordinate of the mesh center.
+        y_thickness (float): Thickness of the mesh in the Y direction.
+
+    Returns:
+        openmc.Tally: The mesh tally object.
+    """
+    mesh = openmc.RegularMesh()
+    mesh.dimension = [bin_number, 1, bin_number]
+    mesh.lower_left = (lower_left[0], y_value - y_thickness / 2, lower_left[1])
+    mesh.upper_right = (upper_right[0], y_value + y_thickness / 2, upper_right[1])
+
+    mesh_filter = openmc.MeshFilter(mesh)
+    mesh_tally = openmc.Tally(name=name_mesh_tally)
+    particle_filter = openmc.ParticleFilter([particule_type])
+    mesh_tally.filters = [mesh_filter, particle_filter]
+    mesh_tally.scores = ['flux']
+    return mesh_tally
+
+
+def mesh_tally_yz(
+    name_mesh_tally="flux_mesh",
+    particule_type='neutrons',
+    bin_number=400,
+    lower_left=(-50.0, -50.0),
+    upper_right=(50.0, 50.0),
+    x_value:float = 0.0,
+    x_thickness:float = 1.0
+    )-> object:
+    """
+    Create a mesh tally in the YZ plane at a specified X value.
+
+    Parameters:
+        name_mesh_tally (str): Name of the tally.
+        particule_type (str): Particle type.
+        bin_number (int): Number of bins in each direction.
+        lower_left (tuple): Lower left corner of the mesh (y, z).
+        upper_right (tuple): Upper right corner of the mesh (y, z).
+        x_value (float): X coordinate of the mesh center.
+        x_thickness (float): Thickness of the mesh in the X direction.
+
+    Returns:
+        openmc.Tally: The mesh tally object.
+    """
+    mesh = openmc.RegularMesh()
+    mesh.dimension = [1, bin_number, bin_number]
+    mesh.lower_left = (x_value - x_thickness / 2, lower_left[0], lower_left[1])
+    mesh.upper_right = (x_value + x_thickness / 2, upper_right[0], upper_right[1])
 
     mesh_filter = openmc.MeshFilter(mesh)
     mesh_tally = openmc.Tally(name=name_mesh_tally)
