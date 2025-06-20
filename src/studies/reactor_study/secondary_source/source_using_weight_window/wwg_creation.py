2c916c68d99699676f22534112dbc66332f8ff7a
simonfouc120 <simon.fcbrt@gmail.com>
2025-06-20 18:19:21 +0200

    feat:add_wwg_using

---
+++ src/studies/reactor_study/secondary_source/source_using_weight_window/wwg_creation.py
@@ -0,0 +1,85 @@
+import openmc
+import os 
+import time
+import json
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from pathlib import Path
+import sys 
+from PIL import Image
+import numpy as np
+
+CWD = Path(__file__).parent.resolve()
+
+project_root = Path(__file__).resolve().parents[5]  
+sys.path.append(str(project_root))
+from parameters.parameters_paths import PATH_TO_CROSS_SECTIONS
+from parameters.parameters_materials import FUEL_MATERIAL, HELIUM_MATERIAL, AIR_MATERIAL, CONCRETE_MATERIAL, GRAPHITE_MATERIAL, STEEL_MATERIAL, WATER_MATERIAL
+from src.utils.pre_processing.pre_processing import (remove_previous_results, mesh_tally_plane)
+from src.utils.post_preocessing.post_processing import load_mesh_tally, load_dammage_energy_tally, load_mesh_tally_dose
+os.environ["OPENMC_CROSS_SECTIONS"] = PATH_TO_CROSS_SECTIONS
+
+from src.models.model_complete_reactor import MODEL, GRAPHITE_CELL, CALCULATION_CELL
+
+MODEL.export_to_xml()
+material = MODEL.materials
+graphite_cell = GRAPHITE_CELL
+geometry = MODEL.geometry
+
+settings = openmc.Settings()
+batches_number= 1
+settings.batches = batches_number
+settings.inactive = 0
+settings.particles = 400000 # try more
+settings.source = openmc.FileSource('surface_source.h5')
+settings.photon_transport = True
+
+mesh = openmc.RegularMesh().from_domain(geometry)
+mesh.dimension = (100, 100, 100)
+mesh.lower_left = (-500.0, -500.0, -500.0)
+mesh.upper_right = (500.0, 500.0, 500.0)
+wwg = openmc.WeightWindowGenerator(
+    mesh=mesh,  # this is the mesh that covers the geometry
+    energy_bounds=np.linspace(0.0, 15e6, 10),  # 10 energy bins from 0 to 15 MeV
+    particle_type='neutron',  # we are interested in photons
+)
+settings.max_history_splits = 1_000  # limit the number of splits to avoid too many histories
+settings.weight_window_generators = wwg
+
+mesh_tally_neutron_yz = mesh_tally_plane(name_mesh_tally = "flux_mesh_neutrons_yz", particule_type='neutron', plane="yz",
+                                      bin_number=800, lower_left=(-450.0, -450.0), upper_right=(450.0, 450.0),
+                                      thickness= 10.0, coord_value=0.0)
+
+# Neutron flux tally on the CALCULATION_CELL
+flux_tally_neutron = openmc.Tally(name="flux_tally_neutron")
+flux_tally_neutron.scores = ['flux']
+flux_tally_neutron.filters = [openmc.ParticleFilter("neutron"), openmc.CellFilter(CALCULATION_CELL)]
+
+tallies = openmc.Tallies([flux_tally_neutron, mesh_tally_neutron_yz])
+
+settings.export_to_xml()
+tallies.export_to_xml()
+
+remove_previous_results(batches_number=batches_number)
+start_time = time.time()
+os.environ["OMP_NUM_THREADS"] = "4"
+ww_statepoint_filename = openmc.run()
+
+openmc.run(threads=4)
+end_time = time.time()
+calculation_time = end_time - start_time
+
+# Save calculation time to a JSON file
+output_json = CWD / "calculation_time.json"
+with open(output_json, "w") as f:
+    json.dump({"calculation_time_seconds": calculation_time}, f)
+
+# Load the statepoint file
+statepoint_file = openmc.StatePoint(f'statepoint.{batches_number}.h5')
+
+# Load the neutron flux tally
+flux_tally_neutron = statepoint_file.get_tally(name="flux_tally_neutron")
+
+
+load_mesh_tally(cwd = CWD, statepoint_file = statepoint_file, name_mesh_tally="flux_mesh_neutrons_yz",particule_type="neutron", bin_number=800,
+                lower_left=(-450.0, -450.0), upper_right=(450.0, 450.0), zoom_x=(-450, 450), zoom_y=(-450, 450), plane="yz", saving_figure=False)
