db80d87ac3c9bf9e1dcf8aaf69d5b5683b668f88
simonfouc120 <simon.fcbrt@gmail.com>
2025-07-13 18:48:24 +0200

    feat:add_wwg_creation_itteration

--- src/studies/reactor_study/secondary_source/source_using_weight_window/wwg_creation/wwg_creation_by_itteration/wwg_creation.py
+++ src/studies/reactor_study/secondary_source/source_using_weight_window/wwg_creation/wwg_creation_by_itteration/wwg_creation.py
@@ -12,7 +12,7 @@ from copy import deepcopy
 
 CWD = Path(__file__).parent.resolve()
 
-project_root = Path(__file__).resolve().parents[6]  
+project_root = Path(__file__).resolve().parents[7]  
 sys.path.append(str(project_root))
 from parameters.parameters_paths import PATH_TO_CROSS_SECTIONS
 from parameters.parameters_materials import FUEL_MATERIAL, HELIUM_MATERIAL, AIR_MATERIAL, CONCRETE_MATERIAL, GRAPHITE_MATERIAL, STEEL_MATERIAL, WATER_MATERIAL
@@ -23,60 +23,61 @@ os.environ["OPENMC_CROSS_SECTIONS"] = PATH_TO_CROSS_SECTIONS
 
 from src.models.model_complete_reactor import MODEL, GRAPHITE_CELL, CALCULATION_CELL
 
-itteration_number = 0
-itteration_max = 4
-
-
-materials = MODEL.materials
-for index,material in enumerate(materials):
-    materials[index] = reducing_density(material, 10)  
-
-MODEL.materials = materials
-MODEL.export_to_xml()
-
-
-geometry = MODEL.geometry
-
-settings = openmc.Settings()
-batches_number= 1
-settings.batches = batches_number
-settings.inactive = 0
-settings.particles = 10000000 # try more
-settings.source = openmc.FileSource('surface_source.h5')
-settings.photon_transport = True
-
-if itteration_number != 0:
-    ww = openmc.hdf5_to_wws("weight_windows.h5")  
-    ww_corrected = create_and_apply_correction_ww_tally(ww, target=np.array([0.0, 400.0, -300.0]), nx=30, ny=30, nz=30,)
-    settings.weight_windows = ww_corrected
-else:
-    pass
-
-mesh = openmc.RegularMesh().from_domain(geometry)
-mesh.dimension = (25, 25, 25)
-mesh.lower_left = (-500.0, -500.0, -500.0)
-mesh.upper_right = (500.0, 500.0, 500.0)
-
-energy_bounds = np.linspace(0.0, 15e6, 10)  # 10 energy bins from 0 to 15 MeV
-
-wwg_neutron = openmc.WeightWindowGenerator(
-    mesh=mesh,  
-    energy_bounds=energy_bounds,
-    particle_type='neutron', 
-    method="magic"
-)
-
-wwg_photon = deepcopy(wwg_neutron)
-wwg_photon.particle_type = 'photon'
-
-settings.max_history_splits = 1_000  
-settings.weight_window_generators = [wwg_neutron, wwg_photon]
-
-settings.export_to_xml()
-
-remove_previous_results(batches_number=batches_number)
+divide_factor_list = [10, 8, 6, 4, 2]
 start_time = time.time()
-openmc.run(threads=4)
+
+for itteration_number, divide_factor in enumerate(divide_factor_list):
+
+    materials = MODEL.materials
+    for index,material in enumerate(materials):
+        materials[index] = reducing_density(material, 10)  
+
+    MODEL.materials = materials
+    MODEL.export_to_xml()
+
+
+    geometry = MODEL.geometry
+
+    settings = openmc.Settings()
+    batches_number= 1
+    settings.batches = batches_number
+    settings.inactive = 0
+    settings.particles = 100000 # try more
+    settings.source = openmc.FileSource('surface_source.h5')
+    settings.photon_transport = True
+
+    if itteration_number != 0:
+        ww = openmc.hdf5_to_wws("weight_windows.h5")  
+        ww_corrected = create_and_apply_correction_ww_tally(ww, target=np.array([0.0, 400.0, -300.0]), nx=25, ny=25, nz=25,)
+        settings.weight_windows = ww_corrected
+    else:
+        pass
+
+    mesh = openmc.RegularMesh().from_domain(geometry)
+    mesh.dimension = (25, 25, 25)
+    mesh.lower_left = (-500.0, -500.0, -500.0)
+    mesh.upper_right = (500.0, 500.0, 500.0)
+
+    energy_bounds = np.linspace(0.0, 15e6, 10)  # 10 energy bins from 0 to 15 MeV
+
+    wwg_neutron = openmc.WeightWindowGenerator(
+        mesh=mesh,  
+        energy_bounds=energy_bounds,
+        particle_type='neutron', 
+        method="magic"
+    )
+
+    wwg_photon = deepcopy(wwg_neutron)
+    wwg_photon.particle_type = 'photon'
+
+    settings.max_history_splits = 1_000  
+    settings.weight_window_generators = [wwg_neutron, wwg_photon]
+
+    settings.export_to_xml()
+
+    remove_previous_results(batches_number=batches_number)
+    openmc.run()
+
 end_time = time.time()
 calculation_time = end_time - start_time
 
