4e79d6ad4ce59a8c154f55173d0da6a7f9d80cf6
simonfouc120 <simon.fcbrt@gmail.com>
2025-10-01 00:12:55 +0200

    feat:add_100cm_wall

---
+++ src/studies/reactor_study/class_model/stop_24h/02_with_ww/01_ww_creation/02_wall_100cm/pilot.py
@@ -0,0 +1,95 @@
+import openmc
+import os 
+import json
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from pathlib import Path
+import sys 
+from PIL import Image
+import numpy as np
+import copy
+
+
+CWD = Path(__file__).parent.resolve() 
+project_root = Path(__file__).resolve().parents[8]  
+sys.path.append(str(project_root))
+
+from parameters.parameters_paths import PATH_TO_CROSS_SECTIONS, IMAGE_PATH
+from parameters.parameters_materials import *
+from src.utils.pre_processing.pre_processing import *
+from src.utils.post_preocessing.post_processing import *
+from src.models.model_complete_reactor_class import *
+
+os.environ["OPENMC_CROSS_SECTIONS"] = PATH_TO_CROSS_SECTIONS
+
+
+for material in material_dict.values():
+    material= reducing_density(material, factor=5)
+
+material_dict["FUEL_UO2_MATERIAL"].remove_element("U")
+
+my_reactor = Reactor_model(materials=material_dict, 
+                           total_height_active_part=500.0, 
+                           light_water_pool=False, 
+                           slab_thickness=100,
+                           concrete_wall_thickness=100,
+                           calculation_sphere_coordinates=(-575, 0, 0),
+                           calculation_sphere_radius=50)
+
+MODEL = my_reactor.model
+
+
+data = np.loadtxt('gamma_spectrum_tabular.txt', comments='#')
+energies_eV = data[:,0]*1e6  # Energy in MeV
+pdf_list = data[:,1]  # PDF values
+photons_per_s = 2.5e15
+
+# -----------------------
+settings = openmc.Settings()
+batches_number= 100
+settings.batches = batches_number
+settings.inactive = 0
+settings.particles = 1000000
+settings.statepoint_interval = 10
+settings.photon_transport = True
+settings.run_mode = "fixed source"
+spatial_dist = openmc.stats.Box((-200., -200, -250.), (200., 200., 250.))
+settings.source = openmc.IndependentSource(
+    space=spatial_dist,
+    constraints={'domains': [GRAPHITE_MATERIAL, HELIUM_MATERIAL, FUEL_UO2_MATERIAL]},  # contraints can be on cells, materials or universe (here both work fine)
+)
+settings.source.particles = ["photon"]
+settings.source.energy = openmc.stats.Discrete(energies_eV, pdf_list)
+settings.source.strength = photons_per_s
+
+mesh = openmc.RegularMesh().from_domain(MODEL.geometry)
+mesh.dimension = (65, 65, 65)
+mesh.lower_left = (-700.0, -700.0, -700.0)
+mesh.upper_right = (700.0, 700.0, 700.0)
+
+wwg_photon = openmc.WeightWindowGenerator(
+    mesh=mesh,  
+    max_realizations=settings.particles, 
+    particle_type='photon', 
+    method="magic"
+)
+
+settings.max_history_splits = 10_000  
+settings.weight_window_generators = [wwg_photon]
+
+MODEL.settings = settings
+MODEL.export_to_xml()
+
+plot_geometry(materials = openmc.Materials(list(my_reactor.material.values())), 
+              plane="xy", origin=(0,0,0), saving_figure=True, dpi=500, height=1700, width=1700,
+              pixels=(700, 700), color_by="cell")
+
+plot_geometry(materials = openmc.Materials(list(my_reactor.material.values())), 
+              plane="xz", origin=(0,0,0), saving_figure=True, dpi=500, height=1700, width=1700,
+              pixels=(700, 700), color_by="cell")
+
+remove_previous_results()
+
+openmc.run()
+
+remove_intermediate_files()
---
+++ src/studies/reactor_study/class_model/stop_24h/02_with_ww/02_ww_using/01_ww_old/post.py
@@ -0,0 +1,62 @@
+import openmc
+import os 
+import json
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from pathlib import Path
+import sys 
+from PIL import Image
+import numpy as np
+
+CWD = Path(__file__).parent.resolve() 
+project_root = Path(__file__).resolve().parents[7]  
+sys.path.append(str(project_root))
+
+from parameters.parameters_paths import PATH_TO_CROSS_SECTIONS, IMAGE_PATH
+from parameters.parameters_materials import *
+from src.utils.pre_processing.pre_processing import *
+from src.utils.post_preocessing.post_processing import *
+from src.models.model_complete_reactor_class import *
+
+os.environ["OPENMC_CROSS_SECTIONS"] = PATH_TO_CROSS_SECTIONS
+
+material_dict["FUEL_UO2_MATERIAL"].remove_element("U")
+
+statepoint = openmc.StatePoint(f"statepoint.100.h5")
+
+bin_mesh_volume = get_mesh_volumes(lower_left=(-850.0, -850.0), upper_right=(850.0, 850.0), thickness=20.0, bin_number=500)
+photons_per_s = 2.5e15
+
+my_reactor = Reactor_model(materials=material_dict, 
+                           total_height_active_part=500.0, 
+                           light_water_pool=False, 
+                           slab_thickness=100,
+                           concrete_wall_thickness=50,
+                           calculation_sphere_coordinates=(-520, 0, 0),
+                           calculation_sphere_radius=50)
+
+MODEL = my_reactor.model
+MODEL.export_to_xml()
+
+dose_over_geometry(model=MODEL ,statepoint_file=statepoint, name_mesh_tally="flux_mesh_photons_xy", plane="xy", 
+                saving_figure=True, bin_number=500, lower_left=(-850.0, -850.0), upper_right=(850.0, 850.0), 
+                zoom_x=(-850, 850), zoom_y=(-850, 850), plot_error=True, particule_type="photon", 
+                particles_per_second=photons_per_s, mesh_bin_volume=bin_mesh_volume, radiological_area=True)   
+
+dose_over_geometry(model=MODEL ,statepoint_file=statepoint, name_mesh_tally="flux_mesh_photons_xz", plane="xz", 
+                saving_figure=True, bin_number=500, lower_left=(-850.0, -850.0), upper_right=(850.0, 850.0), 
+                zoom_x=(-800, 800), zoom_y=(-800, 800), plot_error=True, particule_type="photon", 
+                particles_per_second=photons_per_s, mesh_bin_volume=bin_mesh_volume, pixels_model_geometry=1_000_000)  
+
+volume_tally_sphere = Volume_cell(cell=my_reactor.calc_sphere_cell).get_volume()
+
+dose_rate, dose_rate_error = compute_dose_rate_tally(
+    statepoint=statepoint,
+    tally_name="flux_tally_sphere_dose",
+    particule_per_second=photons_per_s,
+    volume=volume_tally_sphere,
+    unit="mSv/h"
+)
+
+relative_error = (dose_rate_error / dose_rate) * 100 if dose_rate > 0 else 0
+print(f"Dose rate in the calculation sphere: {dose_rate:.2f} mSv/h (relative error: {relative_error:.2f} %)")
